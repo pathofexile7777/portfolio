c1bbff4ad2c93c1501aeac089d644dd94ef82ae6
LIMCHIHUN <lch950223@gmail.com>
2023-02-02 23:50:26 +0900

    test run

--- arbitrage/src/main.rs
+++ arbitrage/src/main.rs
@@ -1,4 +1,5 @@
 use log::{debug, info};
+use tungstenite::client;
 use std::{collections::HashMap, convert::Infallible, sync::Arc};
 use tokio::sync::{mpsc, Mutex};
 use url::Url;
@@ -47,4 +48,48 @@ fn get_binance_streams_url(
 #[tokio::main]
 async fn main() {
     log4rs::init_file("log_config.yaml", Default::default()).unwrap();
+    let f = std::fs::File::open("config.yaml").expect("Could not open file.");
+    let app_config: config::AppConfig = serde_yaml::from_reader(f).expect("Could not read values.");
+
+    let clients: Clients = Arc::new(Mutex::new(HashMap::new()));
+
+    // Configure the websocket route, pass a clone of the clients hashmap to use in the ws_handler function.
+    // Cloning creates a reference to the original clients, then configure cors on all routes.
+    info!("Configuring websocket route");
+    let ws_route = warp::path("ws")
+        .and(warp::ws())
+        .and(with_clients(clients.clone()))
+        .and_then(handlers::ws_handler);
+
+    let routes = ws_route.with(warp::cors().allow_any_origin());
+
+    info!("Connecting to binance stream...");
+    let binance_url = get_binance_streams_url(
+        &app_config.depth_streams, 
+        app_config.update_interval, 
+        app_config.results_limit, 
+    );
+
+    info!("Subscribing to binance: {}", binance_url);
+    let (socket, response) = tungstenite::connect(binance_url).expect("Can't connect.");
+    info!("Connected to binance stream.");
+    debug!("HTTP status code: {}", response.status());
+    debug!("Response headers:");
+    for (ref header, ref header_value) in response.headers() {
+        debug!("- {}: {:?}", header, header_value);
+    }
+
+    // Using spawn we create a new task in a separate green thread, we pass the list of connected clients to the main_worker function
+    // This function contains the data update loop that sends data to all the connected clients in the list
+    info!("Starting update loop");
+    tokio::task::spawn(async move {
+        workers::main_worker(clients.clone(), app_config, socket).await;
+    });
+    info!("Starting server");
+    warp::serve(routes).run(([127, 0, 0, 1], 3000)).await;
+}
+
+// ????
+fn with_clients(clients: Clients) -> impl Filter<Extract = (Clients, ), Error = Infallible> + Clone {
+    warp::any().map(move || clients.clone())
 }
